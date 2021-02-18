51a3cc16b642be425ba0f8c32a44fa3b22b1f823
FangFeiyue <18766131540@163.com>
2021-02-19 00:06:25 +0800

    修复滑动时浏览器控制台报Unable to preventDefault inside passive event listener invocation问题

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -23,7 +23,9 @@ export default {
     nextPage () {
       if (this.reader) this.reader.next()
     },
-    toggleTitleAndMenu () {},
+    toggleTitleAndMenu () {
+      console.log('ddd')
+    },
     initEpub () {
       const url = 'http://192.168.0.103:8083/epub/' + this.fileName + '.epub'
       this.book = new Epub(url)
@@ -37,8 +39,6 @@ export default {
         this.touchStartX = e.changedTouches[0].clientX
         this.timeStamp = e.timeStamp
       }).on('touchend', e => {
-        e.preventDefault()
-        e.stopPropagation()
         this.offsetX = e.changedTouches[0].clientX - this.touchStartX
         this.time = e.timeStamp - this.timeStamp
         if (this.time < 500 && this.offsetX > 40) {
@@ -48,6 +48,8 @@ export default {
         } else {
           this.toggleTitleAndMenu()
         }
+        // e.preventDefault()
+        e.stopPropagation()
       })
     }
   }
