b3fcb25b654d5ddb4b71a8ace37159d951bff37f
FangFeiyue <18766131540@163.com>
2021-02-18 23:20:33 +0800

    实现翻页功能

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -17,13 +17,38 @@ export default {
     this.$store.dispatch('setFileName', fileName).then(() => this.initEpub())
   },
   methods: {
+    prevPage () {
+      if (this.reader) this.reader.prev()
+    },
+    nextPage () {
+      if (this.reader) this.reader.next()
+    },
+    toggleTitleAndMenu () {},
     initEpub () {
       const url = 'http://192.168.0.103:8083/epub/' + this.fileName + '.epub'
       this.book = new Epub(url)
-      this.book.rendation = this.book.renderTo('reader', {
+      this.reader = this.book.renderTo('reader', {
         width: innerWidth,
         height: innerHeight
-      }).display()
+      })
+
+      this.reader.display()
+      this.reader.on('touchstart', e => {
+        this.touchStartX = e.changedTouches[0].clientX
+        this.timeStamp = e.timeStamp
+      }).on('touchend', e => {
+        e.preventDefault()
+        e.stopPropagation()
+        this.offsetX = e.changedTouches[0].clientX - this.touchStartX
+        this.time = e.timeStamp - this.timeStamp
+        if (this.time < 500 && this.offsetX > 40) {
+          this.prevPage()
+        } else if (this.time < 500 && this.offsetX < -40) {
+          this.nextPage()
+        } else {
+          this.toggleTitleAndMenu()
+        }
+      })
     }
   }
 }
