5409b7b9952b7f7b38b16ceacbdc2f3f6ccf8480
FangFeiyue <18766131540@163.com>
2021-03-12 10:54:13 +0800

    书城首页标题布局

---
+++ src/views/store/StoreHome.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="store-home">
+    <search-bar/>
+  </div>
+</template>
+<script>
+import SearchBar from '../../components/home/SearchBar.vue'
+export default {
+  components: {
+    SearchBar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/global.scss';
+  .store {
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+  }
+</style>
