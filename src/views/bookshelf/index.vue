aa9456617f277a88da56bb357d07360dca68a820
FangFeiyue <18766131540@163.com>
2021-03-12 14:46:34 +0800

    书架页面

---
+++ src/views/bookshelf/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="store">
+    书架
+    <footer-guide/>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import FooterGuide from '../../components/footer/Footer.vue'
+export default {
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/global.scss';
+  .store {
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+  }
+</style>
