9dd896b5e59c5ebeed97aafd17aeea7b2e608382
FangFeiyue <18766131540@163.com>
2021-03-12 14:46:18 +0800

    修复切换同个路由重复提示问题；新增书架路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -27,6 +32,10 @@ const routes = [
       path: 'home',
       component: () => import('../views/store/StoreHome.vue')
     }]
+  },
+  {
+    path: '/bookshelf',
+    component: () => import('../views/bookshelf/index.vue')
   }
 ]
 
