c166b4298afd7fa71296f135f8dda0fb30701135
FangFeiyue <18766131540@163.com>
2021-03-13 17:37:12 +0800

    推荐卡片布局

--- src/components/home/FlapCard.vue
+++ src/components/home/FlapCard.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="flap-card-wrapper" v-show="flapCardVisible">
+    <div class="flap-card-bg">
+      <div class="flap-card" v-for="item in flapCardList" :key="item.zIndex" :style="{zIndex: item.zIndex}">
+        <div class="flap-card-circle">
+          <div class="flap-card-semi-circle flap-card-semi-circle-left" :style="semiCricelStyle(item, 'left')"></div>
+          <div class="flap-card-semi-circle flap-card-semi-circle-right" :style="semiCricelStyle(item, 'right')"></div>
+        </div>
+      </div>
+    </div>
     <div class="close-btn-wrapper" @click="handleClick">
       <span class="icon-close"></span>
     </div>
@@ -9,9 +17,77 @@
 import { homeMixin } from '../../mixin/homeMixin'
 export default {
   mixins: [homeMixin],
+  data () {
+    return {
+      flapCardList: [
+        {
+          r: 255,
+          g: 102,
+          _g: 102,
+          b: 159,
+          imgLeft: 'url(' + require('@/assets/images/gift-left.png') + ')',
+          imgRight: 'url(' + require('@/assets/images/gift-right.png') + ')',
+          backgroundSize: '50% 50%',
+          zIndex: 100,
+          rotateDegree: 0
+        },
+        {
+          r: 74,
+          g: 171,
+          _g: 171,
+          b: 255,
+          imgLeft: 'url(' + require('@/assets/images/compass-left.png') + ')',
+          imgRight: 'url(' + require('@/assets/images/compass-right.png') + ')',
+          backgroundSize: '50% 50%',
+          zIndex: 99,
+          rotateDegree: 0
+        },
+        {
+          r: 255,
+          g: 198,
+          _g: 198,
+          b: 102,
+          imgLeft: 'url(' + require('@/assets/images/star-left.png') + ')',
+          imgRight: 'url(' + require('@/assets/images/star-right.png') + ')',
+          backgroundSize: '50% 50%',
+          zIndex: 98,
+          rotateDegree: 0
+        },
+        {
+          r: 255,
+          g: 102,
+          _g: 102,
+          b: 159,
+          imgLeft: 'url(' + require('@/assets/images/heart-left.png') + ')',
+          imgRight: 'url(' + require('@/assets/images/heart-right.png') + ')',
+          backgroundSize: '50% 50%',
+          zIndex: 97,
+          rotateDegree: 0
+        },
+        {
+          r: 59,
+          g: 201,
+          _g: 201,
+          b: 22,
+          imgLeft: 'url(' + require('@/assets/images/crown-left.png') + ')',
+          imgRight: 'url(' + require('@/assets/images/crown-right.png') + ')',
+          backgroundSize: '50% 50%',
+          zIndex: 96,
+          rotateDegree: 0
+        }
+      ]
+    }
+  },
   methods: {
     handleClick () {
       this.setFlapCardVisible(false)
+    },
+    semiCricelStyle (item, direction) {
+      return {
+        backgroundColor: `rgb(${item.r} ,${item.g} ,${item.b})`,
+        backgroundImage: direction === 'left' ? item.imgLeft : item.imgRight,
+        backgroundSize: item.backgroundSize
+      }
     }
   }
 }
@@ -27,6 +103,7 @@ export default {
     z-index: 888;
     width: 100%;
     height: 100%;
+    @include center;
     background-color: rgba(0, 0, 0, 0.6);
     .close-btn-wrapper {
       position: absolute;
@@ -43,6 +120,38 @@ export default {
         background-color: #333;
         color: #fff;
         @include center;
+      }
+    }
+
+    .flap-card-bg {
+      position: relative;
+      width: px2rem(64);
+      height: px2rem(64);
+      border-radius: px2rem(5);
+      background-color: #fff;
+      .flap-card {
+        @include absCenter;
+        width: px2rem(48);
+        height: px2rem(48);
+        .flap-card-circle {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          .flap-card-semi-circle{
+            flex: 0 0 50%;
+            width: 50%;
+            height: 100%;
+            background-repeat: no-repeat;
+          }
+          .flap-card-semi-circle-left {
+            border-radius: px2rem(24) 0 0 px2rem(24);
+            background-position: center right;
+          }
+          .flap-card-semi-circle-right {
+            border-radius: 0 px2rem(24) px2rem(24) 0;
+            background-position: center left;
+          }
+        }
       }
     }
   }
