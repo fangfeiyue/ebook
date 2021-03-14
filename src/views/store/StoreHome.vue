98459706e460b7c1da635c64aba5762ccdc5fc57
FangFeiyue <18766131540@163.com>
2021-03-14 18:25:12 +0800

    猜你喜欢和推荐

--- src/views/store/StoreHome.vue
+++ src/views/store/StoreHome.vue
@@ -6,28 +6,36 @@
       <div class="banner-wrapper">
         <img :src="banner"/>
       </div>
+      <guess-you-like :data="guessYouLike"/>
+      <recommend :data="recommend"/>
     </scroll>
-    <footer-guide></footer-guide>
+    <!-- <footer-guide></footer-guide> -->
   </div>
 </template>
 <script>
 import { homeMixin } from '../../mixin/homeMixin'
 import Scroll from '../../components/common/Scroll.vue'
+import Recommend from '../../components/home/Recommend'
+import GuessYouLike from '../../components/home/GuessYouLike'
 import SearchBar from '../../components/home/SearchBar.vue'
 // import FlapCard from '../../components/home/FlapCard'
-import FooterGuide from '../../components/footer/Footer.vue'
+// import FooterGuide from '../../components/footer/Footer.vue'
 import { getHome } from '../../api/home'
 export default {
   mixins: [homeMixin],
   components: {
     SearchBar,
     Scroll,
+    Recommend,
+    GuessYouLike
     // FlapCard,
-    FooterGuide
+    // FooterGuide
   },
   data () {
     return {
-      banner: null
+      recommend: null,
+      banner: null,
+      guessYouLike: null
     }
   },
   mounted () {
@@ -37,6 +45,8 @@ export default {
         const randomIndex = Math.floor(Math.random() * data.random.length)
         this.randomBook = data.random[randomIndex]
         this.banner = data.banner
+        this.recommend = data.recommend
+        this.guessYouLike = data.guessYouLike
       }
       console.log('res', res)
     })
