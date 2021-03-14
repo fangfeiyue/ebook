fcaf64659b7972cb66717f2b775966995e5b6b6a
FangFeiyue <18766131540@163.com>
2021-03-14 22:39:24 +0800

    no message

--- src/api/home.js
+++ src/api/home.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const BASE_URL = process.env.VUE_APP_BASE_URL
 
-export function getHome() {
+export function getHome () {
   return axios({
     method: 'get',
     url: `${BASE_URL}/book/home`
